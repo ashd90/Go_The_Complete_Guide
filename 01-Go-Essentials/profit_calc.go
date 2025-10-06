7e1144a2c588db1ee2a5a81c1dff86eb4b44a73e
Ashish Deshpande <232292705+ashd90@users.noreply.github.com>
2025-10-06 15:42:19 +0530

    updated go files

--- 01-Go-Essentials/profit_calc.go
+++ 01-Go-Essentials/profit_calc.go
@@ -1,28 +1,27 @@
 package main
 
 import (
- "fmt"
+	"fmt"
 )
 
-func profit(text string){
+func profit() {
 	var revenue float64
 	var expenses float64
 	var taxRate float64
-  
+
 	fmt.Print("Please Enter Total Revenue: ")
 	fmt.Scan(&revenue)
 	fmt.Print("Please Enter Total Expenses: ")
 	fmt.Scan(&expenses)
 	fmt.Print("Please Enter Tax Rate: ")
 	fmt.Scan(&taxRate)
-  
+
 	var ebt float64 = revenue - expenses
-	var profit float64 = ebt * (1- taxRate/100)
-	var ratio = ebt/profit
-  
+	var profit float64 = ebt * (1 - taxRate/100)
+	var ratio = ebt / profit
+
 	fmt.Printf("The Total Earnings Before Tax is: %.2f\n", ebt)
 	fmt.Printf("The Total Earnings After Tax is: %.2f\n", profit)
 	fmt.Printf("The ratio is: %.2f\n", ratio)
-	fmt.Println(text)
-	
-  }
+
+}
