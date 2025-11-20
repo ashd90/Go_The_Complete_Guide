1cb31e47b99ab09a5e53e1ae2dbb99854185a391
Ashish Deshpande <232292705+ashd90@users.noreply.github.com>
2025-11-20 12:03:29 +0530

    added communications.go

---
+++ 01-Go-Essentials/communications.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func presentOptions() {
+	fmt.Println("1. Check the Balance")
+	fmt.Println("2. Deposit Cash")
+	fmt.Println("3. Withdraw Cash")
+	fmt.Println("4. Cancel Transaction")
+}
